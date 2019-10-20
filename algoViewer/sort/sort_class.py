d184370a2cd7cc8a7968e26829c3f66591a79ef3
radupasparuga <radupasparuga25@gmail.com>
2019-10-20 10:59:12 +0300

    added sort class

---
+++ algoViewer/sort/sort_class.py
@@ -0,0 +1,12 @@
+class SortClass:
+    def __init__(self, *argv):
+        self.input = *argv
+
+    def show_sort_array(self):
+        raise Exception("show_sort_array is not implemented in SortClass")
+
+    def sort_array_object(self):
+        raise Exception("sort_array_object is not implemented in SortClass")
+
+    def show_complexity(self):
+        raise Exception("show_complexity is not implemented in SortClass")
