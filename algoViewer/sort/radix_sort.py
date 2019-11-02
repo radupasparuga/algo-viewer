124d19d5c82431933ddc6105f662ef05706eb790
radupasparuga <radupasparuga25@gmail.com>
2019-11-02 13:12:50 +0200

    Added radix sort (need fix)

---
+++ algoViewer/sort/radix_sort.py
@@ -0,0 +1,57 @@
+from sort_class import SortClass
+import constant
+
+class RadixSort(SortClass):
+	def __init__(self, *argv):
+		super().__init__(*argv)
+		print(self.input)
+
+	def counting_sort(self, exp1):
+		n = len(self.input) 
+		# The output array elements that will have sorted arr 
+		output = [0] * (n) 
+		# initialize count array as 0 
+		count = [0] * (10) 
+		# Store count of occurrences in count[] 
+		for i in range(0, n): 
+			index = (self.input[i]/exp1) 
+			count[ (index)%10 ] += 1
+	
+		# Change count[i] so that count[i] now contains actual 
+		#  position of this digit in output array 
+		for i in range(1,10): 
+			count[i] += count[i-1] 
+	
+		# Build the output array 
+		i = n-1
+		while i>=0: 
+			index = (self.input[i]/exp1) 
+			output[ count[ (index)%10 ] - 1] = self.input[i] 
+			count[ (index)%10 ] -= 1
+			i -= 1
+	
+		# Copying the output array to arr[], 
+		# so that arr now contains sorted numbers 
+		i = 0
+		for i in range(0,len(self.input)): 
+			self.input[i] = output[i] 
+	
+	def create_algo(self):
+		# Find the maximum number to know number of digits 
+		max1 = max(self.input) 
+	
+		# Do counting sort for every digit. Note that instead 
+		# of passing digit number, exp is passed. exp is 10^i 
+		# where i is current digit number 
+		exp = 1
+		while max1/exp > 0: 
+			self.counting_sort(exp) 
+			exp *= 10
+
+	def show_sort_arr(self):
+		print(self.input)
+
+
+x = RadixSort(4, 5, 1, 3, 8)
+x.create_algo()
+x.show_sort_arr()
