7849783e27f3988cb3eb4015e87bdf6a7bef54e4
radupasparuga <radupasparuga25@gmail.com>
2019-11-16 11:20:02 +0200

    Now css works pog

--- algoViewer/urls.py
+++ algoViewer/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from algoViewer.views import HomeView
+from algoViewer.views import SortView
 
 urlpatterns = [
     path('', HomeView.as_view()),
+    path('sort', SortView.as_view()),
 ]
--- algoViewer/views.py
+++ algoViewer/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
 	template_name = "home.html"
+
+class SortView(TemplateView):
+	template_name = "sort.html"
