3e5ce485cd8a8639092541f51dee18a2d39c1f6a
radupasparuga <radupasparuga25@gmail.com>
2019-11-23 17:16:44 +0200

    Fix for deployment(hopefully)

--- algoViewer/settings.py
+++ algoViewer/settings.py
@@ -116,10 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://algo-viewer.herokuapp.com/static/'
 
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = "./static/"
+STATIC_ROOT = "./assets/"
