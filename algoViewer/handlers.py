1cac38ef885c0511481f4df1470c1c5d5180f070
radupasparuga <radupasparuga25@gmail.com>
2019-12-05 19:24:34 +0200

    Added regex somehow

--- algoViewer/handlers.py
+++ algoViewer/handlers.py
@@ -1,9 +1,14 @@
 from algoViewer.sort.algo_switcher import create_algo_object
+import re
 
 def sort_file_handler(formData):
   data = formData["file"].read()
   data = str(data)[2:-1]
-  # TODO regex
+  test = re.match(r"([0-9]+?;,\s)*[0-9]+", data)
+  if test:
+    print(test.groups())
+  else:
+    print("false")
   data_parsed = data.split(' ')
   num_array = []
   for string in data_parsed:
