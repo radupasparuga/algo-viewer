d0a0d6a5c098bf7c0d563c243f1866b89ddcfd37
radupasparuga <radupasparuga25@gmail.com>
2019-12-06 18:07:11 +0200

    Got rid of regex

--- algoViewer/handlers.py
+++ algoViewer/handlers.py
@@ -1,19 +1,15 @@
 from algoViewer.sort.algo_switcher import create_algo_object
-import re
 
 def sort_file_handler(formData):
   data = formData["file"].read()
   data = str(data)[2:-1]
-  test = re.match(r"([0-9]+?;,\s)*[0-9]+", data)
-  if test:
-    print(test.groups())
-  else:
-    print("false")
   data_parsed = data.split(' ')
   num_array = []
   for string in data_parsed:
     if string.isdigit():
       num_array.append(int(string))
+    else:
+      print("there's an error")
 
   switcher = create_algo_object(num_array)
   switcher[formData["selection"]].create_algo()
