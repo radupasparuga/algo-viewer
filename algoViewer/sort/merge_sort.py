6f3c0aa26ef2f6376a69660db8e9d97590e1e204
radupasparuga <radupasparuga25@gmail.com>
2020-01-19 19:19:22 +0200

    Removed comment

--- algoViewer/sort/merge_sort.py
+++ algoViewer/sort/merge_sort.py
@@ -1,7 +1,7 @@
 from algoViewer.sort.sort_class import SortClass
 from .constant import MERGE_SORT_C, MERGE_SORT_JAVA, MERGE_SORT_PYTHON
 
-class MergeSort(SortClass): # TODO: make algo work 
+class MergeSort(SortClass):
 	def __init__(self):
 		super().__init__()
 
